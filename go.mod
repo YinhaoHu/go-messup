118c466672f3226463415b3bf4edf572df3cab55
Henry Hoo <976693458@qq.com>
2024-09-19 17:11:48 +0800

    feat: provide pkg

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YinhaoHu/go-messup
+
+go 1.23.1
---
+++ pkg/io.go
@@ -0,0 +1,11 @@
+package pkg
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello")
+}
+
+func Say(content string) {
+	fmt.Printf("Say: %s\n", content)
+}
